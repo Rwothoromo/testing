ce127fdb409ab6870ec0e7b91f035ea1a8fc9543
eli <eli@eli>
2017-06-17 12:31:02 +0300

    Demographics Submits --improved

--- resources/views/patients/patient_form.blade.php
+++ resources/views/patients/patient_form.blade.php
@@ -1,7 +1,7 @@
 <div class="row-fluid">
     <!-- Column -->
     <div class="col-md-4">
-<!--        <div id="number" style="display: none">
+        <div id="number" style="display: none">
              Group 
             <div class="form-group">
                 <label for="insurance_number">Insurance Number</label>
@@ -17,9 +17,9 @@
                 </div>
             </div>
             <br/>
-        </div>-->
+        </div>
         <!--This is the div for the insurance details in case they exist-->
-        <!--<div id="details"></div>-->
+        <div id="details"></div>
 
         <div id="insurance">
             <div class="form-group">
@@ -32,7 +32,7 @@
                 <input type="text" placeholder="Surname e.g. Asiimwe" class="form-control compulsory" id="last_name" name="last_name"/>
             </div>
 
-<!--            <div class="form-group">
+            <div class="form-group">
                 <label style="display: inline" for="sex">Gender</label>
                 <div class="controls">
                     <input type="radio" name="sex" value="Male"/>Male &nbsp;
@@ -41,15 +41,15 @@
             </div>
 
             <div class="form-group">
-                <label for="national_id"><?php // echo 'National ID Number'; ?></label>
+                <label for="national_id">National ID Number</label>
                 <div class="controls">
                     <input class="span10" id="national_id" placeholder="" name="national_id" type="text"/>
                 </div>
-            </div>-->
+            </div>
         </div>
 
-<!--        <div class="form-group">
-            <label for="datebirth"><?php // echo 'Date of Birth'; ?></label>
+        <div class="form-group">
+            <label for="datebirth">Date of Birth</label>
             <div class="controls">
                 <div class="input-append date">
                     <input class="span12 compulsory" required onchange="calculate_age();"  type="text" id="birthdate" name="birthdate"/>
@@ -71,22 +71,18 @@
                 </script>
 
             </div>
-        </div>-->
-        <!-- // Group END -->
-
-        <!-- Group -->
-<!--        <div class="form-group">
+        </div>
+        
+        <div class="form-group">
             <label style="display: inline; font-weight: bold" for="village">Age</label> &nbsp;&nbsp;
             <div class="controls" style="display: inline">
                 Years&nbsp;<input type="number" onfocus="calculate_age();" min="0" name="age" id="age" readonly class="form-control compulsory span3" size="7"/>
                 Months&nbsp;<input name="agemonths" class="span3" onfocus="calculate_months();" type="text" id="agemonths" size="7"/>
             </div>
-        </div>-->
-        <!-- // Group END -->
-
-        <!-- Group -->
-<!--        <div class="form-group">
-            <label for="village"><?php // echo 'Marital Status'; ?></label>
+        </div>
+        
+        <div class="form-group">
+            <label for="maritalstatus">Marital Status</label>
 
             <div class="controls">
                 <input type="radio" name="maritalstatus" value="Single"/>
@@ -99,51 +95,40 @@
                 Other
 
             </div>
-        </div>-->
-        <!-- // Group END -->
-
-        <!-- Group -->
-<!--        <div class="form-group">
-
-            <label class="control-label"><?php // echo 'Religion'; ?></label>
-
+        </div>
+        
+        <div class="form-group">
+            <label class="control-label">Religion</label>
             <div class="controls">
                 <select class="form-control compulsory span9" name="religion" id="religion" required>
                     <option value="1">egsg</option>
                 </select>
                 <a href="#modal-religion" data-toggle="modal"><font size="1">Add new</font></a>
             </div>
-        </div>-->
-        <!-- // Group END -->
-
+        </div>
     </div>
-    <!-- // Column END -->
-
-    <!-- Column -->
+    
     <div class="col-md-4">
         <!-- Group -->
-<!--        <div class="form-group">
-            <label for="village"><?php // echo 'Occupation'; ?></label>
+        <div class="form-group">
+            <label for="occupation">Occupation</label>
             <div class="controls">
                 <select class="form-control compulsory span9" name="occupation" id="occupation">
                 </select>
                 <a href="#modal-occupation" data-toggle="modal"><font size="1">Add new</font></a>
             </div>
-        </div>-->
-        <!-- // Group END -->
-        <!-- Group -->
-<!--        <div class="form-group">
-            <label for="village"><?php // echo 'Next of Kin'; ?></label>
+        </div>
+        
+        <div class="form-group">
+            <label for="village">Next of Kin</label>
 
             <div class="controls"><input class="span10" onchange="show('next_kin_div');" id="nextkin" name="nextkin" type="text"/></div>
-        </div>-->
-        <!-- // Group END -->
-
-        <!-- Group -->
-<!--        <div id="next_kin_div" style="DISPLAY: none">
+        </div>
+        
+        <div id="next_kin_div" style="DISPLAY: none">
             <div class="form-group">
                 <label class="control-label"
-                       for="nextrelation"><?php // echo 'Relationship to next of kin'; ?></label>
+                       for="nextrelation"><?php echo 'Relationship to next of kin'; ?></label>
 
                 <div class="controls" class="col-md-10">
                     <select name="nextrelation" id="nextrelation" width="14px">
@@ -161,53 +146,42 @@
                         <option value="Son">Son</option>
                         <option value="Uncle">Uncle</option>
                         <option value="Wife">Wife</option>
-
-                    </select></div>
+                    </select>
+                </div>
             </div>
-        </div>-->
-        <!-- // Group END -->
-
-
-        <!-- Group -->
-<!--        <div class="form-group">
-            <label for="village"><?php // echo 'Mobile Number'; ?></label>
-
-            <div class="controls"><input class="span10" id="mobilenumber" placeholder="Eg: 07xx123456"
-                                         name="mobilenumber" type="text"/></div>
-        </div>-->
-        <!-- // Group END -->
-
-        <!-- Group -->
-<!--        <div class="form-group">
-            <label for="village"><?php // echo 'Mobile Owner'; ?></label>
+        </div>
+        
+        <div class="form-group">
+            <label for="mobilenumber">Mobile Number</label>
             <div class="controls">
-                <input type="radio" name="mobileowner" onfocus="hide('other_phone_owner_div');" class="checkbox" value="Self"/>
-                Self
-                <input type="radio" name="mobileowner" onfocus="show('other_phone_owner_div');" class="checkbox"  value="Other"/>
-                Other
+                <input class="span10" id="mobilenumber" placeholder="Eg: 07xx123456" name="mobilenumber" type="text"/>
             </div>
-        </div>-->
-        <!-- // Group END -->
-        <!-- Group -->
-<!--        <div class="form-group" id="other_phone_owner_div" style="DISPLAY: none">
+        </div>
+        
+        <div class="form-group">
+            <label style="display: inline" for="mobileowner">Mobile Owner</label>
+            <div class="controls">
+                <input type="radio" name="mobileowner" onfocus="hide('other_phone_owner_div');" value="Self"/>Self &nbsp;
+                <input type="radio" name="mobileowner" onfocus="show('other_phone_owner_div');" value="Other"/>Other &nbsp;
+            </div>
+        </div>
+        
+        <div class="form-group" id="other_phone_owner_div" style="DISPLAY: none">
             <label class="control-label">Owner's Name</label>
             <div class="controls">
                 <input type="text" class="span10 compulsory" name="other_phone_owner" id="other_phone_owner"/>
             </div>
-        </div>-->
-        <!-- // Group END -->
-
-        <!-- Group -->
-<!--        <div class="form-group">
+        </div>
+        
+        <div class="form-group">
             <label class="control-label">Name of LC1 Chairman</label>
 
             <div class="controls">
                 <input type="text" class="span10" name="chairman_name" id="chairman_name"/>
             </div>
-        </div>-->
-        <!-- // Group END -->
-        <!-- Group -->
-<!--        <div class="form-group">
+        </div>
+        
+        <div class="form-group">
             <label class="control-label">Any Contact in <?php // echo HOSP_NAME;     ?></label>
 
             <div class="controls">
@@ -216,11 +190,9 @@
                 <input type="radio" class="form-control compulsory" onfocus="hide('my_div');" name="any_contact" id="any_contact"
                        value="No"/>&nbsp;No
             </div>
-        </div>-->
-        <!-- // Group END -->
-
-<!--        <div id="my_div" style="DISPLAY: none">
-             Group 
+        </div>
+        
+        <div id="my_div" style="DISPLAY: none">
             <div class="form-group">
                 <label class="control-label">Name of contact in <?php // echo HOSP_NAME;     ?></label>
 
@@ -228,14 +200,12 @@
                     <input type="text" class="span10 compulsory" name="kisiizi_contact_name" id="kisiizi_contact_name"/>
                 </div>
             </div>
-             // Group END 
-        </div>-->
+        </div>
 
-        <!-- Group -->
-<!--        <div class="form-group">
-            <label for="patient_category"><?php // echo 'Patient Category'; ?></label>
+        <div class="form-group">
+            <label for="patient_category"><?php echo 'Patient Category'; ?></label>
             <div class="controls">
-                <select class="span10 compulsory" name="patient_category" id="patient_category">-->
+                <select class="span10 compulsory" name="patient_category" id="patient_category">
                     <?php
 //                    $cat_query = mysql_query("select * from patient_category order by Category_Name");
 //                    echo '<option value="">select category</option>';
@@ -247,51 +217,46 @@
 //                    }
 //                    echo $cat_dropdown;
                     ?>
-<!--                </select><br />
+                </select><br />
                 <a href="#modal-patient_category" data-toggle="modal"><font size="1">Add new</font></a>
             </div>
-        </div>-->
-        <!-- // Group END -->
-
+        </div>
     </div>
-    <!-- // Column END -->
-
-    <!--<div class="col-md-4">-->
+    
+    <div class="col-md-4">
         <!-- Group -->
-<!--        <div class="form-group">
+        <div class="form-group">
             <label class="control-label">Language Preference</label>
 
             <div class="controls">
                 <select name="language" required class="form-control compulsory span9">
-                    <option value="">-- select --</option>
+                    <!--<option value="">-- select --</option>-->
                     <option value="vern" selected>Vernacular</option>
                     <option value="eng">English</option>
                 </select>
             </div>
-        </div>-->
-        <!-- // Group END -->
-        <!-- Group -->
-<!--        <div class="form-group">
-            <label class="control-label">Patient Number</label>
+        </div>
+        
+        <div class="form-group">
+            <!--<label class="control-label">Patient Number</label>-->
 
             <div class="controls">
                 <input readonly class="form-control compulsory span8" value="<?php // echo $newPatId;     ?>" id="patientnumber"
                        name="patientnumber" type="hidden"/>
             </div>
-        </div>-->
-        <!-- // Group END -->
+        </div>
+        
+        <input type="hidden" name="staff_id" value="<?php // echo $_SESSION['user_id'];     ?>">
 
-        <!--<input type="hidden" name="staff_id" value="<?php // echo $_SESSION['user_id'];     ?>">-->
+        <?php if (isset($_GET["pat_id"])) { ?>
 
-        <?php // if (isset($_GET["pat_id"])) { ?>
-
-<!--            <input type="hidden" name="from_pay_roll" value="0">
+            <input type="hidden" name="from_pay_roll" value="0">
 
             <div class="form-group">
                 <label for="position">Position</label>
                 <div class="controls">
                     <select class="span9 compulsory" required name="position" id="position">
-                        <?php // echo custom_dropdown("staff_positions", 'Position_Id', 'Position_Name', ""); ?>
+                        <?php echo custom_dropdown("staff_positions", 'Position_Id', 'Position_Name', ""); ?>
                     </select>
                     <a href="#modal-position" data-toggle="modal"><font size="1">Add new</font></a>
                 </div>
@@ -301,26 +266,24 @@
                 <label for="department">Department</label>
                 <div class="controls">
                     <select class="span9 compulsory" required name="department" id="department">
-                        <?php // echo custom_dropdown("departments", 'Department_Id', 'Department_Name', ""); ?>
+                        <?php echo custom_dropdown("departments", 'Department_Id', 'Department_Name', ""); ?>
                     </select>
                     <a href="#modal-department" data-toggle="modal"><font size="1">Add new</font></a>
                 </div>
-            </div>-->
+            </div>
 
-        <?php // } ?>
-        <!-- Group -->
-        <!-- To be used in Kisiizi -->
-        <!---->
-<!--        <div class="form-group">
+        <?php } ?>
+        
+        <div class="form-group">
             <label for="district">Residence</label>
             <div class="controls">
                 <select class="span9 compulsory" name="district_id" id="district_id">
                 </select>
                 <a href="#modal-district" data-toggle="modal"><font size="1">Add new</font></a>
             </div>
-        </div>-->
+        </div>
 
-<!--        <span id="wait_1" style="display: none;">
+        <span id="wait_1" style="display: none;">
             <img alt="Please Wait" src="pages/patients/ajax-loader.gif"/>
         </span>
         <span id="result_1" style="display: none;"></span>
@@ -338,10 +301,8 @@
         <span id="wait_4" style="display: none;">
             <img alt="Please Wait" src="pages/patients/ajax-loader.gif"/>
         </span>
-        <span id="result_4" style="display: none;"></span>-->
-
-
-    <!--</div>-->
+        <span id="result_4" style="display: none;"></span>
+    </div>
 
 </div>
 
